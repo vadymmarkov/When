03e304c80950c8b1d662981291708d2f2bccdd87
Vadym Markov <markov.vadym@gmail.com>
2016-02-07 14:46:55 +0100

    Resolve and revoke

--- Sources/Shared/Promise.swift
+++ Sources/Shared/Promise.swift
@@ -6,8 +6,31 @@ enum State<T> {
 
 class Promise<T> {
 
-  typealias Executor = (resolve: (object: T) -> Void, revoke: (error: ErrorType) -> Void) -> Void
+  typealias Executor = (resolve: (instance: T) -> Void, revoke: (error: ErrorType) -> Void) -> Void
 
   var state = State<T>.Pending
   var executor: Executor
+  var fulfillment: T?
+  var error: ErrorType?
+
+  init(executor: Executor) {
+    self.executor = executor
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+      self.executor(resolve: self.resolve, revoke: self.revoke)
+    })
+  }
+
+  func resolve(instance: T) {
+    if state == .Pending {
+      fulfillment = instance
+      state = .Fulfilled
+    }
+  }
+
+  func revoke(error: ErrorType) {
+    if state == .Pending {
+      self.error = error
+      state = .Rejected
+    }
+  }
 }
