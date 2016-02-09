c7ecc6b04d7a705f6470b1ab69ed6646e31e5e7d
Vadym Markov <markov.vadym@gmail.com>
2016-02-09 01:20:10 +0100

    Helper function + When

--- Sources/Shared/Functions.swift
+++ Sources/Shared/Functions.swift
@@ -1,9 +1,42 @@
-//
-//  Functions.swift
-//  When
-//
-//  Created by Vadym Markov on 09/02/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import Foundation
+
+func backgroundQueue() -> dispatch_queue_t {
+  return dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0)
+}
+
+func serialQueue() -> dispatch_queue_t {
+  return dispatch_queue_create("When.SerialQueue", DISPATCH_QUEUE_SERIAL)
+}
+
+public func when<T>(promises: [Promise<T>]) -> Promise<[String: T]> {
+  let masterPromise = Promise<[String: T]>()
+  var values = [String: T]()
+
+  var (total, resolved) = (promises.count, 0)
+
+  promises.forEach { promise in
+    promise.then(
+      doneFilter: { value -> State<T>? in
+        dispatch_sync(masterPromise.queue) {
+          resolved++
+          values[promise.key] = value
+
+          if resolved == total {
+            masterPromise.resolve(values)
+          }
+        }
+
+        return nil
+      },
+      failFilter: { error -> State<T>? in
+        dispatch_sync(masterPromise.queue) {
+          masterPromise.reject(error)
+        }
+
+        return nil
+      }
+    )
+  }
+
+  return masterPromise
+}
