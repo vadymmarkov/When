27aa17a58a58a6bd4a8a4d609b00dce444dee3b1
Vadym Markov <markov.vadym@gmail.com>
2016-02-09 01:19:53 +0100

    Base promises functionality

--- Sources/Shared/Promise.swift
+++ Sources/Shared/Promise.swift
@@ -1,36 +1,140 @@
 import Foundation
 
-enum State<T> {
-  case Pending, Fulfilled, Rejected
+public final class Promise<T> {
+
+  public typealias DoneHandler = T -> Void
+  public typealias FailureHandler = ErrorType -> Void
+  public typealias CompletionHandler = Result<T> -> Void
+
+  public let key = NSUUID().UUIDString
+
+  var queue: dispatch_queue_t
+  private(set) var state: State<T>
+
+  private var observer: Observer<T>?
+  private var doneHandler: DoneHandler?
+  private var failureHandler: FailureHandler?
+  private var completionHandler: CompletionHandler?
+
+  // MARK: - Initialization
+
+  public init(state: State<T> = .Pending, queue: dispatch_queue_t = serialQueue()) {
+    self.state = state
+    self.queue = queue
+  }
+
+  // MARK: - States
+
+  public func reject(error: ErrorType) {
+    dispatch_sync(queue) {
+      guard self.state.isPending() else {
+        return
+      }
+
+      let state: State<T> = .Rejected(error: error)
+      self.notify(state: state)
+    }
+  }
+
+  public func resolve(value: T) {
+    dispatch_sync(queue) {
+      guard self.state.isPending() else {
+        return
+      }
+
+      let state: State<T> = .Resolved(value: value)
+      self.notify(state: state)
+    }
+  }
+
+  // MARK: - Callbacks
+
+  public func done(handler: DoneHandler) -> Self {
+    doneHandler = handler
+    return self
+  }
+
+  public func fail(handler: FailureHandler) -> Self {
+    failureHandler = handler
+    return self
+  }
+
+  public func always(handler: CompletionHandler) -> Self {
+    completionHandler = handler
+    return self
+  }
+
+  // MARK: - Helpers
+
+  private func notify(state state: State<T>?) {
+    guard let state = state, result = state.result else {
+      return
+    }
+
+    self.state = state
+
+    switch result {
+    case let .Success(value):
+      doneHandler?(value)
+    case let .Failure(error):
+      failureHandler?(error)
+    }
+
+    completionHandler?(result)
+
+    if let observer = observer {
+      dispatch_async(observer.queue) {
+        observer.notify(result)
+      }
+    }
+
+    observer = nil
+  }
 }
 
-class Promise<T> {
+// MARK: - Then
 
-  typealias Executor = (resolve: (instance: T) -> Void, revoke: (error: ErrorType) -> Void) -> Void
+extension Promise {
 
-  var state = State<T>.Pending
-  var executor: Executor
-  var fulfillment: T?
-  var error: ErrorType?
+  public func then<U>(
+    queue: dispatch_queue_t = backgroundQueue(),
+    doneFilter: (T -> State<U>?)? = nil,
+    failFilter: (ErrorType -> State<U>?)? = nil) -> Promise<U> {
 
-  init(executor: Executor) {
-    self.executor = executor
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
-      self.executor(resolve: self.resolve, revoke: self.revoke)
-    })
+      let promise = Promise<U>()
+
+      observer = Observer(queue: queue) { result in
+        switch result {
+        case let .Success(value):
+          promise.notify(state: doneFilter?(value))
+        case let .Failure(error):
+          promise.notify(state: failFilter?(error))
+        }
+      }
+
+      notify(state: state)
+
+      return promise
   }
 
-  func resolve(instance: T) {
-    if state == .Pending {
-      fulfillment = instance
-      state = .Fulfilled
-    }
-  }
+  public func then<U>(
+    queue: dispatch_queue_t = backgroundQueue(),
+    doneFilter: ((T, Promise<U>) -> Void)? = nil,
+    failFilter: ((ErrorType, Promise<U>) -> Void)? = nil) -> Promise<U> {
 
-  func revoke(error: ErrorType) {
-    if state == .Pending {
-      self.error = error
-      state = .Rejected
-    }
+      let promise = Promise<U>()
+
+      observer = Observer(queue: queue) { result in
+        switch result {
+        case let .Success(value):
+          doneFilter?(value, promise)
+        case let .Failure(error):
+          failFilter?(error, promise)
+        }
+      }
+
+      notify(state: state)
+
+      return promise
   }
 }
