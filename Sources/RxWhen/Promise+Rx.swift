ae0e48fc1129a57c9efc82a5fb682473a2b289c3
Vadym Markov <markov.vadym@gmail.com>
2017-01-31 21:57:42 +0100

    Add promise extension

---
+++ Sources/RxWhen/Promise+Rx.swift
@@ -0,0 +1,24 @@
+import When
+import RxSwift
+
+// MARK: - Observable
+
+extension Promise: ObservableConvertibleType {
+
+  public func asObservable() -> Observable<T> {
+    return Observable.create({ observer in
+      self
+        .done({ value in
+          observer.onNext(value)
+        })
+        .fail({ error in
+          observer.onError(error)
+        })
+        .always({ _ in
+          observer.onCompleted()
+        })
+
+      return Disposables.create()
+    })
+  }
+}
