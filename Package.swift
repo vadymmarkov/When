17a7bfdb10e0c61f1f82a95c150ad6113bc71eaa
Serhii Yuryk <yurykserhii@gmail.com>
2020-01-21 01:10:03 +0200

    Manifest code style updated.

--- Package.swift
+++ Package.swift
@@ -9,14 +9,21 @@ let package = Package(
         .tvOS(.v9)
     ],
     products: [
-        .library(name: "When", targets: ["When"]),
-    ], dependencies: [
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMinor(from: "2.2.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMinor(from: "8.0.5"))
+        .library(name: "When",
+                 targets: ["When"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Quick/Quick.git",
+                 .upToNextMinor(from: "2.2.0")),
+        .package(url: "https://github.com/Quick/Nimble.git",
+                 .upToNextMinor(from: "8.0.5"))
     ],
     targets: [
-        .target(name: "When", path: "Sources"),
-        .testTarget(name: "WhenTests", dependencies: ["When", "Quick", "Nimble"], path: "WhenTests"),
+        .target(name: "When",
+                path: "Sources"),
+        .testTarget(name: "WhenTests",
+                    dependencies: ["When", "Quick", "Nimble"],
+                    path: "WhenTests"),
     ],
     swiftLanguageVersions: [.v5]
 )
